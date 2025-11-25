46dd2fc16ca4acb0afaecf8c5b805c70b8c35227
kaitlbiz9752-sudo <k.aitlbiz9752@uca.ac.ma>
2025-11-25 18:30:45 +0100

    first commit

---
+++ ens-springbash/scripts/logs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Affichage des 30 dernières lignes de logs :"
+tail -n 30 logs/app.log
---
+++ ens-springbash/scripts/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Démarrage de l'application Spring Boot..."
+
+mkdir -p logs
+
+nohup mvn spring-boot:run > logs/app.log 2>&1 &
+
+echo "Application démarrée avec succès (PID : $!)"
